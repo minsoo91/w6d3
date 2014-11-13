091ad92c5ee09e072ba513fc4c2504dbc91d4512
App Academy Student <student@appacademy.io>
2014-11-12 17:36:18 -0800

    Adding snake awesomeness.

---
+++ snake_game/js/snake-view.js
@@ -0,0 +1,40 @@
+(function () {
+  if (typeof Snakes === "undefined") {
+    window.Snakes = {};
+  }
+  var View = Snakes.View = function ($el) {
+    this.$el = $el;
+    this.board = new Snakes.Board();
+    this.listener = this.bindKeyHandlers();
+    setInterval(this.step.bind(this), 300);
+  };
+  
+  View.prototype.bindKeyHandlers = function () {
+    var that = this;
+    this.$el.on("keydown", function(event) {
+      console.log("key!");
+      that.handleKeyEvent(event);
+    });
+  }
+  
+  // View.prototype.render = function(){
+  //
+  // }
+  View.prototype.step = function () {
+    this.board.tick();
+    this.$el.html("<pre>" + this.board.render() + "</pre>");
+  }
+  
+  View.prototype.handleKeyEvent = function (event) {
+    console.log("in keyhandler!");
+    if (event.keyCode === 37) {
+      this.board.snake.turn("W");
+    } else if (event.keyCode === 38) {
+      this.board.snake.turn("N");
+    } else if (event.keyCode === 39) {
+      this.board.snake.turn("E");
+    } else if (event.keyCode === 40) {
+      this.board.snake.turn("S");
+    }
+  }
+})();
---
+++ snake_game/js/snake.js
@@ -0,0 +1,88 @@
+(function () {
+  if (typeof Snakes === "undefined") {
+    window.Snakes = {};
+  }
+  
+  var Snake = Snakes.Snake = function () {
+    this.segments = [new Snakes.Coord(5, 5)];
+    this.dir = "N";
+  };
+  
+  Snake.prototype.move = function () {
+    this.segments.unshift(this.segments[0].plus(this.dir));
+    this.segments.pop();
+  };
+  
+  Snake.prototype.turn = function (newDir) {
+    this.dir = newDir;
+  };
+  
+  var Coord = Snakes.Coord = function (row, col) {
+    this.row = row;
+    this.col = col;
+  };
+  
+  Coord.prototype.plus = function (dir) {
+    var row = this.row;
+    var col = this.col;
+    if (dir === "N") {
+      row = this.row - 1;
+    } else if (dir === "S") {
+      row = this.row + 1;
+    } else if (dir === "E") {
+      col = this.col + 1;
+    } else if (dir === "W") {
+      col = this.col - 1;
+    }
+    
+    return new Coord(row, col);
+  };
+  
+  var Board = Snakes.Board = function () {
+    this.snake = new Snake();
+    this.grid = Board.makeGrid();
+  }
+  
+  Board.makeGrid = function () {
+    var grid = [];
+
+    for (var i = 0; i < 10; i++) {
+      grid.push([]);
+      for (var j = 0; j < 10; j++) {
+        grid[i].push(null);
+      }
+    }
+
+    return grid;
+  };
+  //
+  //if typeof Coord(i, j).exists? in this.snake.segment
+  
+  Board.prototype.render = function () {
+    var megaString = "";
+    console.log(this.snake.segments);
+    for (var i = 0; i < 10; i++) {
+      for (var j = 0; j < 10; j++) {
+        if (this.grid[i][j] === null) {
+            megaString += " . ";
+        } else if (this.grid[i][j] == "S"){
+          megaString += " S ";
+        }
+      }
+      megaString += '\n';
+    }
+    return megaString;
+  };
+  
+  Board.prototype.tick = function(){
+    this.snake.move();
+    this.grid = Board.makeGrid();
+    var that = this;
+    this.snake.segments.forEach(function(segment){
+      that.grid[segment.row][segment.col] = "S"
+    })
+    //snake has moved, update grid...
+  };
+  
+})();
+
